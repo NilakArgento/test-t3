5dd6b55e625403dcf20d71bb078d08e386559bad
Calin Staver <calin.staver@gmail.com>
2026-02-26 16:51:16 +0200

    pass some tests

--- test.js
+++ test.js
@@ -8,4 +8,19 @@ test('Validation of checkMinimStep logic', async (t) => {
         assert.strictEqual(checkMinimStep(5, 7, 123), "YES");
     });
 
+    await t.test('Case (9, 7, 55) should be YES', () => {
+        assert.strictEqual(checkMinimStep(9, 7, 55), "YES");
+    });
+
+    await t.test('Case (10, 5, 7) should be NO', () => {
+        assert.strictEqual(checkMinimStep(10, 5, 7), "NO");
+    });
+
+    await t.test('Case (4, 8, 123) should be NO', () => {
+        assert.strictEqual(checkMinimStep(4, 8, 123), "NO");
+    });
+
+    await t.test('Case (2, 4, 10) should be YES', () => {
+        assert.strictEqual(checkMinimStep(2, 4, 10), "YES");
+    });
 });
